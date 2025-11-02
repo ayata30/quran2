5cc589f01b0698e46a7e354d576f728c321fe012
ayata30 <ayataadams@gmail.com>
2025-11-02 12:20:20 -0700

    Updated a bit of the ui

--- qurandetect-frontend/src/App.js
+++ qurandetect-frontend/src/App.js
@@ -1,11 +1,21 @@
+import Header from'./Header.jsx';  
+import Footer from'./Footer.jsx';  
 import Recorder from "./components/Recorder";
-
+import React from 'react';
+import './App.css';
+import './index.css';
 function App() {
+  
   return (
+    <>
+     <Header />
     <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h1>QuranDetect Demo</h1>
+      <h1>🎙️Quran Detect</h1>
+      <h3> Find verses of the Quran through QD.</h3>
       <Recorder />
     </div>
+         <Footer />
+    </>
   );
 }
 
---
+++ qurandetect-frontend/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return (
+        <footer>
+            <p>&copy;{new Date().getFullYear()} QuranDetect</p>
+        </footer>
+    );
+}
+
+export default Footer
+
