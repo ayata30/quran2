08cd58ff157e62604e1c40f90a86bda8b0a29bf0
ayata30 <ayataadams@gmail.com>
2026-02-11 05:02:41 -0700

    updates

--- backend/main.py
+++ backend/main.py
@@ -90,8 +90,6 @@ async def detect(file: UploadFile = File(...)):
 
 PORT = int(os.environ.get("PORT", 8080))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+
 
     
